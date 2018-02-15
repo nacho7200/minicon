79ea9d6cc4d79afad2a0828f534c58ec6cc3f332
Masamichi Nagao <26617754+nacho7200@users.noreply.github.com>
2018-02-15 10:05:02 +0000

    2_15_last

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -18,4 +18,21 @@ class Admin::EventsController < ApplicationController
     @event = Event.new(event_params)
     @event.save
   end
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    @event.update(event_params)
+    redirect_to admin_event_path(@event), notice: 'Event was successfully updated.'
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to admin_events_url, notice: 'Event was successfully destroyed.'
+  end
 end
