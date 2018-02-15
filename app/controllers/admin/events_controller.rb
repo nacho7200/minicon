21f36fcc786ed4f7da9905e17ab0823049e627ca
Masamichi Nagao <26617754+nacho7200@users.noreply.github.com>
2018-02-15 08:48:21 +0000

    create new category

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,21 @@
+class Admin::EventsController < ApplicationController
+  layout 'admin'
+    
+  def index
+    @events = Event.all
+  end
+  
+  def show
+    @event = Event.find(params[:id])
+  end
+  
+  def new
+    @event = Event.new
+  end
+  
+  def create
+    event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    @event = Event.new(event_params)
+    @event.save
+  end
+end
